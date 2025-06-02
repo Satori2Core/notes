461ec33f37d172a398b06d7df5aab284c0fb2195
mortal-wker <mortal_scode@163.com>
2025-06-02 21:08:03 +0800

    初始化 VuePress 网站

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	title: 'Satori2Core 知识库',
+	description: '知道自己在做什么，才能写出有灵魂的代码',
+	// 
+	themeConfig: {
+		// 导航栏
+		nav: [
+			{text: '首页', link: '/'},
+			{text: '关于', link: '/about'}
+		]
+	},
+	dest: 'docs/.vuepress/dist'	// 构建输出目录
+}
